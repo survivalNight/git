20d7e1264ee2ab1c834a8404a220a55961d34b9e
TATA <yuantata2001@gmail.com>
2022-10-29 21:31:50 +0800

    second commit

--- test/src/main/java/com/atguigu/git/git.java
+++ test/src/main/java/com/atguigu/git/git.java
@@ -4,6 +4,8 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello git !");
         System.out.println("hello git !");
+        System.out.println("hello git 2 !");
+        System.out.println("hello git 2 !");
 
     }
 }
