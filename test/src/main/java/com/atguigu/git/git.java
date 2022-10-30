70e939ff0c5dc1fd2845b60a7aa9a1ec6a5c2500
TATA <yuantata2001@gmail.com>
2022-10-30 16:02:18 +0800

    push commit

--- test/src/main/java/com/atguigu/git/git.java
+++ test/src/main/java/com/atguigu/git/git.java
@@ -11,6 +11,7 @@ public class git {
         System.out.println("hello git 4 ");
         System.out.println("master test ");
         System.out.println("hot-fix test ");
+        System.out.println("push test ");
 
     }
 }
